85bf585595046fff2287b96705d9b5eaf7956985
Tayyip İlhan <tilhan@damlayayinevi.com.tr>
2020-01-18 09:36:55 +0300

    deneme

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -129,12 +129,7 @@ jQuery(document).ready(function($){
         }
 
         lastScrollTop = st;
-    }
-    
-    
-    $('.site-content').css('margin-top', $('header').outerHeight() + 'px');
-    
-    
+    }    
     
     // to top
     $("a.sscroll[href='#totop']").click(function() {
